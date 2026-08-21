521716f0b29f1a5ca285b6378da27a026c02b84b
thefloowp <thefloowp@gmail.com>
2026-08-21 09:57:54 +0800

    Create layout.jsx

---
+++ app/admin/layout.jsx
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: {
+    default: "Admin | Floowp",
+    template: "%s | Floowp Admin",
+  },
+};
+
+export default function AdminLayout({ children }) {
+  return children;
+}
